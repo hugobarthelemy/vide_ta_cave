10194c638f344cc1be31bf42402ee9e65aa9f768
hugo barthelemy <barthelemyhugo@gmail.com>
2017-10-06 19:06:54 +0200

    add form for create a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,21 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+    @categories = Category.all
+  end
+
+  def create
+    product = Product.new(product_params)
+    product.user_id = current_user.id
+    product.save!
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'products/index'
+  resources :products, only: [ :index, :new, :create ]
 
   devise_for :users
   root to: 'pages#home'
