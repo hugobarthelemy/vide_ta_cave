b9a87c0136b1803b986c37b5b201dbfe491b2589
hugo barthelemy <barthelemyhugo@gmail.com>
2017-10-09 17:47:41 +0200

    change test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,12 +2,6 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    Product.all.each do |product|
-      print product.name
-      print product.category.name
-      p "######"
-      print product.user.city.name
-    end
     get products_index_url
     assert_response :success
   end
