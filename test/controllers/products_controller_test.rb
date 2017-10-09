8ed62128327fbb16581c69cc128d682a6c2a6753
hugo barthelemy <barthelemyhugo@gmail.com>
2017-10-09 17:08:25 +0200

    add link between tables

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,9 +2,13 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    login_as users(:george)
+    Product.all.each do |product|
+      print product.name
+      print product.category.name
+      p "######"
+      print product.user.city.name
+    end
     get products_index_url
     assert_response :success
   end
-
 end
